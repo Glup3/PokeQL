1138631a07493aff26946779f041545d1ce4ef60
Phuc Tran <glup3.tran@gmail.com>
2019-04-19 22:23:59 +0200

    resolve berry firmness resource

--- server/graphql/berries/berry_firmness/berry_firmness.js
+++ server/graphql/berries/berry_firmness/berry_firmness.js
@@ -2,6 +2,14 @@ exports.resolver = {
   Query: {
     berryFirmness: (root, { id, name }, { dataSources }) => {
       return dataSources.pokeAPI.getEndpointByNameOrID('berry-firmness', id, name);
+    },
+    berryFirmnesses: (root, { perPage, page }, { dataSources }, info) => {
+      return dataSources.pokeAPI.getEndpointList('berry-firmness', perPage, page);
+    },
+  },
+  BerryFirmness: {
+    berries: ({ berries }, idk, { dataSources }) => {
+      return dataSources.pokeAPI.resolveAPIResourceList(berries);
     }
   }
 }
