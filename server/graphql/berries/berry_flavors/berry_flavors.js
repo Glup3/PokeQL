6b153bf286f87c9fc4703f96de619a32fa87c26b
Phuc Tran <glup3.tran@gmail.com>
2019-04-19 22:27:40 +0200

    resolve berry flavor resource

--- server/graphql/berries/berry_flavors/berry_flavors.js
+++ server/graphql/berries/berry_flavors/berry_flavors.js
@@ -2,9 +2,14 @@ exports.resolver = {
   Query: {
     berryFlavor: (root, { id, name }, { dataSources }) => {
       return dataSources.pokeAPI.getEndpointByNameOrID('berry-flavor', id, name);
-    }
+    },
+    berryFlavors: (root, { perPage, page }, { dataSources }, info) => {
+      return dataSources.pokeAPI.getEndpointList('berry-flavor', perPage, page);
+    },
   },
   BerryFlavor: {
-    contestType: ({ contest_type }) => contest_type
+    contestType: ({ contest_type }, idk, { dataSources }) => {
+      return dataSources.pokeAPI.resolveAPIResource(contest_type);
+    }
   }
 }
